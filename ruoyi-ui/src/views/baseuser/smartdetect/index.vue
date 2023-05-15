d609a2ceabf2252c378aca99d4edc35ccdf36824
ylyhappy <2778335106@qq.com>
2023-05-15 20:20:26 +0800

    增加智能检测页面

---
+++ ruoyi-ui/src/views/baseuser/smartdetect/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <video class="video-js source-video-box" muted loop controls>
+      <source :src="sourceVideo" type="video/mp4">
+    </video>
+    <video class="video-js target-video-box" muted loop controls>
+      <source :src="detectVideo" type="video/mp4">
+    </video>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "index.vue",
+  data() {
+    return {
+      sourceVideo: this.$route.query.video_url,
+      detectVideo: this.$route.query.detect_video_url
+    }
+  }
+}
+
+</script>
+
+<style scoped>
+.source-video-box, .target-video-box {
+  width: 840px;
+  height: 460px;
+}
+
+</style>
